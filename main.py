ff0b72d9c8fdc30010cbc2df42f9de1657aa02f4
BilalIssmail <bilal.issmail@gmail.com>
2023-07-28 16:52:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+####################
+#Display Main Menu:#
+####################
+
+
 n = 0 # setting a counter for admin attempts
 def dispMenu(n):
   # taking user input
@@ -9,11 +14,11 @@ def dispMenu(n):
 
     if password == "": #blank password indicate normal user
       print("Welcome " + user_name + "!")
-      print("def dispUserMenu")
+      dispUserMenu()
 
     elif user_name == "admin" and password == "admin123123":
       print ("Welcome admin!")
-      print("def dispAdMenu")
+      dispAdMenu()
     else: # entering any password is considered an admin attempt
       print("You've entered a wrong password. Please enter the correct admin password or leave password blank if you want to book a ticket.")
       n += 1 # incrementing the attempt counter
@@ -23,4 +28,60 @@ def dispMenu(n):
   else:
     print("You have reached maximum attempt limit.")
 
-dispMenu(n)
+
+#####################
+#Display Admin Menu:#
+#####################
+
+
+def dispAdMenu():
+  admin_input = input("""Please select the number of your choice:
+1. Display Statistics")
+2. Book a Ticket
+3. Display all Tickets
+4. Change Ticket’s Priority
+5. Disable Ticket
+6. Run Events
+7. Exit
+"""  )
+  if admin_input == 1:
+    dispStat()
+    dispAdMenu()
+  elif admin_input == 2:
+    bookTicket()
+    dispAdMenu()
+  elif admin_input == 3:
+    dispAllTickets()
+    dispAdMenu()
+  elif admin_input == 4:
+    changePriority()
+    dispAdMenu
+  elif admin_input == 5:
+    disableTicket()
+    dispAdMenu()
+  elif admin_input == 6:
+    runEvent()
+    dispAdMenu()
+  elif admin_input == 7:
+    print("You have logged out.")
+    dispMenu(n)
+
+
+###########################
+#Display Normal User Menu:#
+###########################
+
+
+def dispUserMenu():
+  user_input = input("""Please select the number of your choice:
+  1. Book a ticket
+  2. Exit
+  """)
+  if user_input == 1:
+    bookTicket()
+    print("""Your ticket has been successfully booked.
+    You can book another ticket or exit.""")
+    dispUserMenu()
+
+
+###############
